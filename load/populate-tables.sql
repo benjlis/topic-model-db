0f52740fb37c605fc4d277c317526a38bb10a606
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-07-20 13:03:33 -0400

    Corrected error on insert into topic_tokens in populate-tables.sql.

--- load/populate-tables.sql
+++ load/populate-tables.sql
@@ -9,4 +9,4 @@ insert into topic_docs(topic_id, doc_id, score)
 select topic_id, doc_id, topic_score from topic_doc_stage;
 
 insert into topic_tokens(topic_id, token_id, score)
-select id, topic, beta from topic_tokens_stage; 
+select topic, id, beta from topic_token_stage;
