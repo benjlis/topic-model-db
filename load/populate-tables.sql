45721ec8926ad67f3ce0b4353f3418584dad6cd9
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-02-09 16:01:34 -0500

    Refactoring changes based on analysis

--- load/populate-tables.sql
+++ load/populate-tables.sql
@@ -1,6 +1,6 @@
 -- populates the topic model tables from stage
-insert into topics(topic_id, topic)
-select id, name from topics_stage;
+insert into topics(topic_id, title, name)
+select id, title, name from topics_stage;
 
 insert into topic_docs(topic_id, doc_id, score)
 select topic_id, doc_id, topic_score from topic_doc_stage;
