50491c8b84a6393df57be492514132df01163569
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-02-09 15:52:31 -0500

    Added comments on removal of token-related tables and inserts

--- load/populate-tables.sql
+++ load/populate-tables.sql
@@ -1,12 +1,13 @@
 -- populates the topic model tables from stage
-insert into tokens(token_id, token)
-select id, value from tokens_stage;
-
 insert into topics(topic_id, topic)
 select id, name from topics_stage;
 
 insert into topic_docs(topic_id, doc_id, score)
 select topic_id, doc_id, topic_score from topic_doc_stage;
 
-insert into topic_tokens(topic_id, token_id, score)
-select topic, id, beta from topic_token_stage;
+-- TBD remove tokens and topic_token
+-- insert into tokens(token_id, token)
+-- select id, value from tokens_stage;
+--
+-- insert into topic_tokens(topic_id, token_id, score)
+-- select topic, id, beta from topic_token_stage;
