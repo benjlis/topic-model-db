8d526bc0c2d2384bd265929e410bc48b02b70d20
Ben Lis <bl2759@columbia.edu>
2024-09-04 08:30:01 -0400

    Removed commented out DDL

--- load/create-stage-tables.sql
+++ load/create-stage-tables.sql
@@ -12,13 +12,3 @@ create table topic_doc_stage (
          topicsum        float,
          top75           integer
 );
--- TBD remove tokens_stage and topic_token_stage
--- create table tokens_stage (
---	 value 	 text,
---	 id 	 integer
---);
--- create table topic_token_stage (
---         topic   integer,
---         beta    float,
---         id      integer
---);
