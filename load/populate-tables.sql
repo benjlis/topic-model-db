b549fbd6d4ed2ef9a68a3e6c3d7f6a8439d86f91
Ben Lis <bl2759@columbia.edu>
2024-09-05 10:07:30 -0400

    Added corpus field; removed obsolete DML

--- load/populate-tables.sql
+++ load/populate-tables.sql
@@ -1,13 +1,9 @@
 -- populates the topic model tables from stage
-insert into topics(topic_id, title, name)
-select id, title, name from topics_stage;
+insert into topics(corpus, topic_id, title, name)
+select :'corpus', id, title, name 
+    from topics_stage;
 
-insert into topic_docs(topic_id, doc_id, score)
-select topic_id, doc_id, topic_score from topic_doc_stage;
+insert into topic_docs(corpus, topic_id, doc_id, score)
+select :'corpus', topic_id, doc_id, topic_score 
+    from topic_doc_stage;
 
--- TBD remove tokens and topic_token
--- insert into tokens(token_id, token)
--- select id, value from tokens_stage;
---
--- insert into topic_tokens(topic_id, token_id, score)
--- select topic, id, beta from topic_token_stage;
