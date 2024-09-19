38b79158399be83cfc8e75d97b28ff6837a30e35
Ben Lis <bl2759@columbia.edu>
2024-09-19 08:18:54 -0400

    Added  new format for csv_file

--- update-topic-model.sh
+++ update-topic-model.sh
@@ -30,7 +30,7 @@ heading () {
 load_stage ()
 {
     table_name="foiarchive.${1}_stage"
-    csv_file="${DATA_DIR}/${CORPUS}_${1}.csv"
+    csv_file="${DATA_DIR}/${CORPUS}_update_${1}.csv"
     if [ ! -f "$csv_file" ]; then
         echo "Error: File ${csv_file} not found."
         exit 1
