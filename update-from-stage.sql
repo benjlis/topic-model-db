da96c3316954aa861ede3de8a320f483a65b6831
Ben Lis <bl2759@columbia.edu>
2024-09-19 09:07:41 -0400

    Added foiarchive schema reference to staging tables

--- update-from-stage.sql
+++ update-from-stage.sql
@@ -8,9 +8,9 @@ delete from foiarchive.topics where corpus = :'corpus';
 -- populates the topic model tables from stage
 insert into foiarchive.topics(corpus, topic_id, title, name)
 select :'corpus', id, title, name 
-    from topics_stage;
+    from foiarchive.topics_stage;
 
 insert into foiarchive.topic_docs(corpus, topic_id, doc_id, score)
 select :'corpus', topic_id, doc_id, topic_score 
-    from topic_doc_stage;
+    from foiarchive.topic_doc_stage;
 commit;
